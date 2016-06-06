0294b8af4f5e3cfe99f1aa02dd1717ee7fa0912e
Shadilan <shadilan@gmail.com>
2016-06-07 00:24:33 +0300

    AddVersion

--- src/main/java/google/Version.java
+++ src/main/java/google/Version.java
@@ -8,17 +8,15 @@ import utility.StringUtils;
  */
 public class Version {
     public static boolean checkVersion(String version){
-        return "2.0.18a".equals(version) ||
-                "2.0.19a".equals(version)|| "3.0.0a".equals(version);
+        return  "3.0.0a".equals(version) ||
+                "3.0.1a".equals(version);
     }
     public static boolean checkHash(String hash,String version,String operation,String... params){
         String code;
         switch (version){
-            case "2.0.18a":code="COWBOW";
-                break;
-            case "2.0.19a":code="COWBOW";
-                break;
             case "3.0.0a":code="COWBOW";
+                break;
+            case "3.0.1a":code="COWBOW";
                 break;
             default: return false;
         }
