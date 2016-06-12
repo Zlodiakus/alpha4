4e420a9c72de77f9fedccdb596ebfa85ddeb904c
Shadilan <shadilan@gmail.com>
2016-06-12 22:56:54 +0300

    Intel

--- src/main/webapp/intel/intel.js
+++ src/main/webapp/intel/intel.js
@@ -122,7 +122,7 @@ function getData(){
                             origin: new google.maps.Point(0, 0),
                             // The anchor for this image is the base of the flagpole at (0, 32).
                             anchor: new google.maps.Point(0, 32),
-                            scaledSize: new google.maps.Size(25, 25)
+                            scaledSize: new google.maps.Size(64, 64)
                           };
                         city.mark = new google.maps.Marker({
                             position: {lat: city.lat, lng: city.lng},
