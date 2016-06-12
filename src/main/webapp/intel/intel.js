5a31345cb1279cf0dd267e53898523e83b367455
Shadilan <shadilan@gmail.com>
2016-06-12 21:57:34 +0300

    AddAuthoriceProc

--- src/main/webapp/intel/intel.js
+++ src/main/webapp/intel/intel.js
@@ -70,24 +70,54 @@ function authorize(googleToken){
     xmlhttp.send();
 }
 
-var data;
+var player={};
+var cities=[];
 
 function getData(){
     if (token!='0' && token!=''){
         var map_bounds=map.getBounds();
-    	var minlat=map_bounds.getSouthWest().lat()*1E6;
-    	var minlng=map_bounds.getSouthWest().lng()*1E6;
-    	var maxlat=map_bounds.getNorthEast().lat()*1E6;
-    	var maxlng=map_bounds.getNorthEast().lng()*1E6;
+    	var minlat=Math.round(map_bounds.getSouthWest().lat()*1E6);
+    	var minlng=Math.round(map_bounds.getSouthWest().lng()*1E6);
+    	var maxlat=Math.round(map_bounds.getNorthEast().lat()*1E6);
+    	var maxlng=Math.round(map_bounds.getNorthEast().lng()*1E6);
         xmlhttp = getXmlHttp();
-        console.log("/intel/api.jsp?ReqName=GetData&Token="+token+"&Lat1="+minlat+"&Lng1="+minlng+"&Lat2="+maxlat+"&Lng2="+maxlng);
-        xmlhttp.open("/intel/api.jsp?ReqName=GetData&Token="+token+"&Lat1="+minlat+"&Lng1="+minlng+"&Lat2="+maxlat+"&Lng2="+maxlng, true);
+        console.log("/intel/api.jsp?ReqName=GetData&Token="+token+"&StartLat="+minlat+"&StartLng="+minlng+"&FinishLat="+maxlat+"&FinishLng="+maxlng);
+        xmlhttp.open("/intel/api.jsp?ReqName=GetData&Token="+token+"&StartLat="+minlat+"&StartLng="+minlng+"&FinishLat="+maxlat+"&FinishLng="+maxlng, true);
         xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState == 4) {
                 if(xmlhttp.status == 200) {
                     var data=JSON.parse(xmlhttp.responseText);
+                    player.gold=data.Gold;
+                    player.race=data.Race;
+                    player.GUID=data.GUID;
+                    player.exp=data.Exp;
+                    player.name=data.Name;
+                    if (cities!=null){
+                        cities.forEach(function(currentVal,index,arr){
+                           currentVal.mark.setMap(null);
+                        });
+                        cities=null;
+                        data.Cities.forEach(function(currentVal,index,arr){
+                          var city={};
+                          city.lat=currentVal.Lat/1e6;
+                          city.lng=currentVal.Lng/1e6;
+                          city.level=currentVal.Level;
+                          city.up=currentVal.Upgrade;
+                          city.name=currentVal.Name;
+                          city.race=currentVal.Faction;
+                          city.mark = new google.maps.Marker({
+                              position: {lat: city.lat, lng: city.lng},
+                              title: city.name;
+                              map: map
+                            });
+                            cities.push(city);
+
+                        })
+                    }
+                    for ()
                     console.log(data);
+
                     console.log(xmlhttp.responseText);
                 } else
                 console.log(xmlhttp);
