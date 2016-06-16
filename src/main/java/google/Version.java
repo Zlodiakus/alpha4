3c917778ace2886de637ff1c4cc0933b6699a6be
Shadilan <shadilan@gmail.com>
2016-06-16 09:34:39 +0300

    3.0.4a версия

--- src/main/java/google/Version.java
+++ src/main/java/google/Version.java
@@ -8,22 +8,22 @@ import utility.StringUtils;
  */
 public class Version {
     public static boolean checkVersion(String version){
-        return  "3.0.0a".equals(version) ||
-                "3.0.1a".equals(version)||
+        return  "3.0.1a".equals(version)||
                 "3.0.2a".equals(version)||
-                "3.0.3a".equals(version)
+                "3.0.3a".equals(version)||
+                "3.0.4a".equals(version)
                 ;
     }
     public static boolean checkHash(String hash,String version,String operation,String... params){
         String code;
         switch (version){
-            case "3.0.0a":code="COWBOW";
-                break;
             case "3.0.1a":code="COWBOW";
                 break;
             case "3.0.2a":code="COWBOW";
                 break;
             case "3.0.3a":code="COWBOW";
+                break;
+            case "3.0.4a":code="COWBOW";
                 break;
             default: return false;
         }
